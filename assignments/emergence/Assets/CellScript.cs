a19e0a350b4a87fe5bb760475e75e11c18a03baa
gretchencallahan <gc0051a@student.american.edu>
2023-10-02 18:04:46 -0400

    Adding fun changes to GOL and a lot of comments

--- assignments/emergence/Assets/CellScript.cs
+++ assignments/emergence/Assets/CellScript.cs
@@ -1,27 +1,49 @@
+// Gretchen Callahan
+// Game Programming
+// 03_emergence
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CellScript : MonoBehaviour
 {
-    public bool alive = false;
-    public int x, z;
-    public int aliveNeighbors;
-    public Color livingColor;
-    public Color deadColor;
-    Renderer cellRend;
-    
+    // this is a section for public global variables I used throughout this project
+    public bool alive = false; // keeping track of a cell's state
+    public bool previouslyDead = false; // keeping track of a previous death state for a given cell
+    public int x, z; // x and z values to use for placing each cell in it's spot on the gameboard
+    public int aliveNeighbors; // a variable to keep track of a cell's alive neighbors at a given time
+    public Color livingColor; // a color for living cells (that haven't been previously dead)
+    public Color deadColor; // a color for dead cells
+    Renderer cellRend; // a variable for the cells' renderers
+
     // Start is called before the first frame update
     void Start()
-    {
+    {   
+        // accessing each cell's renderer and storing it here
         cellRend = gameObject.GetComponentInChildren<Renderer>();
+        // changing the color of cells at the start of the game to see alive vs dead cells before the games first iteration
         changeColor();  
     }
 
+    // a function to change a cell's color
     public void changeColor(){
+        // if it's alive go down this conditional
         if(alive){ 
+            if (previouslyDead){
+            // this is a new rule I created to change the color of an alive cell if it has been previously dead
+            // i randomly chose the numbers 23, 67, and 5 to make the colors more interesting
+            // to make the colors more fun I also used the sin function to have them bounce back and forth on the color spectrum
+                float num = Random.value * 23;
+                float hue = Mathf.Abs(Mathf.Sin(num / 67 * 5));
+                cellRend.material.color = Color.HSVToRGB(hue % 2f, 1f, 0.8f);
+            }
+            else{
+            // if the cell hasn't died previously give it the regular color for living cells
             cellRend.material.color = livingColor; 
+            }
         }
+        // if the cell isn't alive go down this conditional and give it the color for dead cells
         else{ 
             cellRend.material.color = deadColor; 
         }
--- assignments/emergence/Assets/GameOfLife.cs
+++ assignments/emergence/Assets/GameOfLife.cs
@@ -1,91 +1,127 @@
+// Gretchen Callahan
+// Game Programming
+// 03_emergence
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameOfLife : MonoBehaviour
 {   
+    // a section for public global variables i used throughout this project
+    // creating a cell game object
     public GameObject cell;
+    // creating a 2d array of cell scripts to use (called cells)
     public CellScript[,] cells;
-    public int boardLength = 30;
+    // an integer value for the length of the original board
+        // now if you want to change the length you have to do it in unity
+    public int boardLength = 40;
     
     // Start is called before the first frame update
     void Start()
-    {
+    {   
+        // generate the board for each game (i made it a square)
         generateBoard(boardLength, boardLength);
     }
 
-    void generateBoard(int n, int m){
+    // a function for generating the gameboards
+    void generateBoard(int m, int n){
         
-        cells = new CellScript[n, m];
-        for(int x = 0; x < n; x++){
-            for(int z = 0; z < m; z++){
-                
+        // make the cells array of the size m x n
+        cells = new CellScript[m, n];
+
+        // using nested for loops instantiate the cells
+        for(int x = 0; x < m; x++){
+            for(int z = 0; z < n; z++){
+
+                // use the cells' transform position to find where they are in unity
                 Vector3 pos = transform.position;
+                // a float value for how far apart the board cells should be spaces
+                    // right now it's 1 because I liked the way it looked like an 8bit game
                 float boardSpacing = 1f;
+                // update the cells' x and y positions using the indices from the loops
                 pos.x = pos.x + x * boardSpacing;
                 pos.z = pos.z + z * boardSpacing;
-                GameObject cellObject = Instantiate(cell, pos, transform.rotation);
                 
+                // create a gameobject for the cells and instantiate them using the cell prefab, pos vairable, and it's transform rotation (currently none)
+                GameObject cellObject = Instantiate(cell, pos, transform.rotation);
+                // for each cell access it's CellScript component
                 cells[x, z] = cellObject.GetComponent<CellScript>();
+                // then using a 50% probability of alive vs dead update the cells' state variable
                 cells[x, z].alive = (Random.value < 0.5f);
+                // then update the cells' x and z variables
                 cells[x, z].x = x;
-                cells[x, z].z = z; }}
-        
+                cells[x, z].z = z; 
+                
+                }
+            }
     }
     
+    // a function for calculating how many of a cell's neighbors are alive
     void aliveNeighbors(){
+        // the max variable here is used to help see if the index is going to go out of bounds when you try to locate a cell's neighbors
+        // this is useful so you don't get errors when you're looking at cells that are on the edges of the board
         int max = boardLength;
+        // in this for loop "i" is going to be used for the x position
         for(int i = 0; i < max; i++){
+            // and "j" will be used for the z position
             for(int j = 0; j < max; j++){
+                // at the start of every inner loop reset the variable for the number of alive neighbors to 0
                 int numAlive = 0;
+                // if i+1 is less then the max index of the board then you have a viable neighbor to the right
                 if(i + 1 < max){ // middle right neighbor
+                    // if that neighbor is alive then increment the number of alive neighbors by 1
                     if(cells[i+1, j].alive == true){
-                        numAlive++; }
-                }
+                        numAlive++; }}
+                // if j+1 is less then the max length of the board then you have another viable neighbor and can increment the count
                 if(j + 1 < max){ // bottom middle neighbor
                     if(cells[i, j+1].alive == true){
-                       numAlive++; }
-                }
+                       numAlive++; }}
+                // with the same logic, if i-1 is greater then 0, the minimum possible index for any cell on the board,
+                // then you have a viable neighbor and can increment the count by 1
                 if(i - 1 >= 0){ // middle left neighbor
                     if(cells[i-1, j].alive == true){
-                        numAlive++; }     
-                }
+                        numAlive++; }}
+                // this works the same for j
                 if(j - 1 >= 0){ // top middle neighbor
                     if(cells[i, j-1].alive == true){
-                        numAlive++; }
-                }
+                        numAlive++; }}
+                // now when you want to look at the neighbors in the corners you have to use both i and j indices
+                // and the logic works the same
                 if(i + 1 < max && j + 1 < max){ // bottom right neighbor
                     if(cells[i+1, j+1].alive == true){
-                        numAlive++; }
-                }
+                        numAlive++; }}
                 if(i + 1 < max && j - 1 >= 0){ // top right neighbor
                     if(cells[i+1, j-1].alive == true){
-                        numAlive++; }
-                }
+                        numAlive++; }}
                 if(i - 1 >= 0 && j + 1 < max){ // bottom left neighbor
                     if(cells[i-1, j+1].alive == true){
-                        numAlive++; }    
-                }
+                        numAlive++; }}
                 if(i - 1 >= 0 && j - 1 >= 0){ // top left neighbor
                     if(cells[i-1, j-1].alive == true){
-                        numAlive++; }    
-                }
-                
+                        numAlive++; }}
+                // when that's all said and done you can set the cell's variable for storing it's # of alive neighbors to the count that you got after all those conditionals
                 cells[i, j].aliveNeighbors = numAlive;
             }    
         }    
     }
     
+    // this is a function that implements the rules of the game of life
     void gameRules(){
+        // you use nested for loops again to access both indices in the cell array
         for(int a = 0; a < boardLength; a++){
             for(int b = 0; b < boardLength; b++){
-                
+                // if the cell at this index is alive you to check if it has between 2 adn 3 neighbors, if so it stays alive
+                // otherwise you use a conditional to set it's state to false(dead) if it has more than 3 or less than 2 neighbors
+                // this is also when I change the variable for previouslyDead to true, to signify that the cell has died at some point, then I change it's color correspondingly
                 if(cells[a, b].alive == true){
                     if(cells[a, b].aliveNeighbors > 3 || cells[a, b].aliveNeighbors < 2){
                         cells[a, b].alive = false;
+                        cells[a, b].previouslyDead = true;
                         cells[a, b].changeColor();
-                    }
-                }    
+                }}    
+                // if the cell is dead all you have to check is if it has exactly 3 neighbors
+                // if so then the cell becomes alive again and you can change it's color again
                 else{
                     if(cells[a, b].aliveNeighbors == 3){
                         cells[a, b].alive = true;
@@ -96,8 +132,10 @@ public class GameOfLife : MonoBehaviour
         }
         
     }
-    
+
     // Update is called once per frame
+    // in my update function I call the functions for checking for alive neighbors and running the rules of the game every new frame
+    // this way the game updates pretty fast and you can see a lot of changes happening to the board.
     void Update()
     {
         aliveNeighbors();
