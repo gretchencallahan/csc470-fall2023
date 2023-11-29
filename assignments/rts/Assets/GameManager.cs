96aedcbbb9684fb41df146ca7f448eeadfcd0068
gretchencallahan <gc0051a@student.american.edu>
2023-11-28 21:06:30 -0500

    Reuploading final build!
    I finished the project

--- assignments/rts/Assets/GameManager.cs
+++ assignments/rts/Assets/GameManager.cs
@@ -13,7 +13,6 @@ public class GameManager : MonoBehaviour
     ObjectScript selectedObject;
     
     public GameObject character;
-    public GameObject canvas;
     
     public GameObject roadPrefab;
     public GameObject housePrefab;
@@ -32,17 +31,18 @@ public class GameManager : MonoBehaviour
     
     
         
-    public void SelectObj(ObjectScript obj)
-    {
-        // Deselect any units that think they are selected
-        foreach (ObjectScript ob in objectPool) {
-            ob.selected = false;
-        }
-        selectedObject = obj;
-        selectedObject.selected = true;
+    //public ObjectScript SelectObj(ObjectScript obj)
+    //{   
+        //// Deselect any units that think they are selected
+        //foreach (ObjectScript ob in objectPool) {
+            //ob.selected = false;
+        //}
+        //selectedObject = obj;
+        //selectedObject.selected = true;
 
-        //UnitSelectedHappened?.Invoke(unit);
-    }
+        ////UnitSelectedHappened?.Invoke(unit);
+        //return selectedObject;
+    //}
     
     
     
@@ -65,12 +65,34 @@ public class GameManager : MonoBehaviour
     {
         if(money > 0){
         
-        
-        
             if(Input.GetMouseButtonDown(0)){
             
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 RaycastHit hit;
+                
+                if(Physics.Raycast(ray, out hit, 999999, ~LayerMask.NameToLayer("objects"))){
+                    //Vector3 closePos = new Vector3 (100000, 0, 100000);
+                    float currentMaxDist = 10000f;
+                    ObjectScript closestObj = null;
+                    foreach(ObjectScript o in objectPool){
+                        float dist = Vector3.Distance(o.transform.position, hit.point);
+                        if(dist < currentMaxDist){
+                            closestObj = o;
+                        }
+                    }
+                    if(Input.GetKey(KeyCode.Q)){
+                        closestObj.transform.Rotate(0, 90, 0, Space.Self);
+                    }
+                    if(Input.GetKey(KeyCode.E)){
+                        closestObj.transform.Rotate(0, -90, 0, Space.Self);    
+                    }
+                    if(Input.GetKey(KeyCode.Delete) || Input.GetKey(KeyCode.Backspace)){
+                        Destroy(closestObj.gameObject);
+                    }
+                    //ObjectScript selectedo = SelectObj(closestObj);
+                    //Debug.Log(closestObj);
+                }
+                
                 if(Physics.Raycast(ray, out hit, 999999, ~LayerMask.NameToLayer("Ground"))){
                 
                 if(selectedObject == null){
@@ -78,63 +100,63 @@ public class GameManager : MonoBehaviour
                     if(Input.GetKey(KeyCode.Alpha1)){
                         // roads are worth 5
                         GameObject newObject = Instantiate(roadPrefab, hit.point, Quaternion.identity);
-                        //objectPool.Add(newObject);
+                        objectPool.Add(newObject.GetComponent<ObjectScript>());
                         money -= 5;
                         moneyText.text = money.ToString();
                     }
                     else if(Input.GetKey(KeyCode.Alpha2)){
                         // houses are worth 20
                         GameObject newObject = Instantiate(housePrefab, hit.point, Quaternion.identity);
-                        //objectPool.Add(newObject);
+                        objectPool.Add(newObject.GetComponent<ObjectScript>());
                         money -= 20;
                         moneyText.text = money.ToString();
                     }
                     else if(Input.GetKey(KeyCode.Alpha3)){
                         // apartments are worth 25
                         GameObject newObject = Instantiate(aptPrefab, hit.point, Quaternion.identity);
-                        //objectPool.Add(newObject);
+                        objectPool.Add(newObject.GetComponent<ObjectScript>());
                         money -= 25;
                         moneyText.text = money.ToString();
                     }
                     else if(Input.GetKey(KeyCode.Alpha4)){
                         // bakerys are worth 15
                         GameObject newObject = Instantiate(bakeryPrefab, hit.point, Quaternion.identity);
-                        //objectPool.Add(newObject);
+                        objectPool.Add(newObject.GetComponent<ObjectScript>());
                         money -= 15;
                         moneyText.text = money.ToString();
                     }
                     else if(Input.GetKey(KeyCode.Alpha5)){
                         // gazebos are worth 15
                         GameObject newObject = Instantiate(gazeboPrefab, hit.point, Quaternion.identity);
-                        //objectPool.Add(newObject);
+                        objectPool.Add(newObject.GetComponent<ObjectScript>());
                         money -= 15;
                         moneyText.text = money.ToString();
                     }
                     else if(Input.GetKey(KeyCode.Alpha6)){
                         // trees are worth 10
                         GameObject newObject = Instantiate(treePrefab, hit.point, Quaternion.identity); 
-                        //objectPool.Add(newObject);
+                        objectPool.Add(newObject.GetComponent<ObjectScript>());
                         money -= 10;
                         moneyText.text = money.ToString();
                     }
                     else if(Input.GetKey(KeyCode.Alpha7)){
                         // benchs are worth 5
                         GameObject newObject = Instantiate(benchPrefab, hit.point, Quaternion.identity);
-                        //objectPool.Add(newObject);
+                        objectPool.Add(newObject.GetComponent<ObjectScript>());
                         money -= 5;
                         moneyText.text = money.ToString();
                     }
                     else if(Input.GetKey(KeyCode.Alpha8)){
                         // squirrels are worth 5
                         GameObject newObject = Instantiate(squirrelPrefab, hit.point, Quaternion.identity);
-                        //objectPool.Add(newObject);
+                        objectPool.Add(newObject.GetComponent<ObjectScript>());
                         money -= 5;
                         moneyText.text = money.ToString();
                     }
                     
                 } 
                 }
-                else{}
+                
             }
         }
         else{
@@ -142,7 +164,7 @@ public class GameManager : MonoBehaviour
             winText.text = "Game over!";
             //particleSystem.Emit();
             //particleSystem.Play();
-            GameObject sparkle = Instantiate(sparklePrefab, canvas.transform.position, Quaternion.identity);
+            //GameObject sparkle = Instantiate(sparklePrefab, canvas.transform.position, Quaternion.identity);
             //Destroy(sparkle, 8);
         }
     }
--- assignments/rts/Assets/ObjectScript.cs
+++ assignments/rts/Assets/ObjectScript.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class ObjectScript : MonoBehaviour
 {
     public bool selected = false;
+    //int clicks = 0;
+    public GameObject thisObj;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        //thisObj = this;
     }
 
     // Update is called once per frame
@@ -18,8 +20,23 @@ public class ObjectScript : MonoBehaviour
         
     }
     
-    private void OnMouseDown()
-    {
-        GameManager.SharedInstance.SelectObj(this);
-    }
+    //private void OnMouseDown()
+    //{
+        
+        
+        //if(clicks % 2 == 0){
+            //ObjectScript selectedObj = GameManager.SharedInstance.SelectObj(this);
+            //selectedObj.selected = true;
+            //if(selectedObj.selected == true){
+                //if(Input.GetKey(KeyCode.Q)){
+                    //selectedObj.transform.localEulerAngles = new Vector3 (0, 90, 0);
+                //}
+                //if(Input.GetKey(KeyCode.E)){
+                    //selectedObj.transform.localEulerAngles = new Vector3 (0, -90, 0);
+                //}
+            //}
+        //}
+        
+        //clicks += 1;
+    //} 
 }
